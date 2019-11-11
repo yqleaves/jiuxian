2d5b5267e56f4b126d11b64d2f5984e9ecb1901b
BK1918yeqi <1747072817@qq.com>
2019-11-11 14:52:25 +0800

    项目初始化

---
+++ src/utils/request.js
@@ -0,0 +1,28 @@
+import axios from "axios"
+
+const server = axios.create({
+    timeout:5000,
+
+    withCredentials:true,
+})
+
+server.interceptors.request.use((config)=>{
+    if(config.methods == "get"){
+        config.params = {...config.data};
+    }
+
+    // config.headers["content-type"] = "application/json";
+},(err)=>{
+    return Promise.reject(err);
+})
+
+server.interceptors.reponse.use((res)=>{
+    if(res.status == 200){
+        return res.data;
+    }
+},(err)=>{
+    return Promise.reject(err);
+})
+
+
+export default server
---
+++ vue.config.js
@@ -0,0 +1,33 @@
+const path  = require('path');
+
+module.exports = {
+    devServer:{
+        proxy:{
+            "/m_v1":{
+                target:"https://m.jiuxian.com",
+                changeOrigin:true
+            }
+        }
+    },
+    // 别名配置
+    configureWebpack:{
+        resolve:{
+            alias:{
+                "@":path.join(__dirname,"./src"),
+                "@api":path.join(__dirname,"./src/api"),
+                "@assets":path.join(__dirname,"./src/assets"),
+                "@common":path.join(__dirname,"./src/common"),
+                "@router":path.join(__dirname,"./src/router"),
+                "@store":path.join(__dirname,"./src/store"),
+                "@router":path.join(__dirname,"./src/router"),
+                "@lib":path.join(__dirname,"./src/lib"),
+                "@pages":path.join(__dirname,"./src/pages"),
+                "@utils":path.join(__dirname,"./src/utils"),
+
+            }
+        }
+    }
+
+}
+
+// /promote/qgajax.do?t=1573453803273&pagenum=1&tabnum=1
