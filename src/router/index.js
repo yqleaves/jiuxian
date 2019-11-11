167694d0a10f0fa43547fe2fb5f2ce6848a307e6
BK1918jiangxiaoxue <1563406794@qq.com>
2019-11-12 07:43:59 +0800

    sort路由删除

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,2 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
 
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-        mode:"hash",
-        routes:[
-            {
-                path:"/sort",
-                component:_=>import("@pages/sort"),
-                name:"sort",
-                meta:{
-                    flag:false
-                }
-            },
-            
-        ]
-})
-
-export default router;
 
